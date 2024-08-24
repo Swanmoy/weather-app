5b1f63423d57416865ecc196abff4db5df9cedc9
Swanmoy <nag.ratul1@gmail.com>
2024-08-24 20:41:52 +0530

    completed development

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import "../styles/error.css";
+import { useNavigate } from "react-router-dom";
 const Error = () => {
-  return <div> Error</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="error-container">
+        <p className="error-text">Opps Something went Wrong!!!</p>
+        <div className="go-home-button" onClick={() => navigate("/")}>
+          <i
+            className="bi bi-arrow-left"
+            style={{ color: "white", fontSize: "30px" }}
+          ></i>
+          <p className="go-back-text"> {"    "}Home</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Error;
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -22,6 +22,27 @@ const Weather = () => {
     const year = date.getFullYear().toString().slice(-2);
     return `${time}-${dayOfWeek}, ${day} ${month}, ${year}`;
   };
+  const formatDate = (input) => {
+    const date = new Date(input);
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const day = date.getDate();
+    const month = monthNames[date.getMonth()];
+    const year = date.getFullYear().toString().slice(-2);
+    return `${day} ${month}, ${year}`;
+  };
   useEffect(() => {
     axios
       .get("http://api.weatherapi.com/v1/forecast.json", {
@@ -61,8 +82,21 @@ const Weather = () => {
             };
           }
         );
+        let tempForecastWeather = data.forecast.forecastday.map(
+          (forecastData) => {
+            return {
+              maxtemp_c: forecastData.day.maxtemp_c,
+              maxtemp_f: forecastData.day.maxtemp_f,
+              mintemp_c: forecastData.day.mintemp_c,
+              mintemp_f: forecastData.day.mintemp_f,
+              icon: forecastData.day.condition.icon,
+              time: formatDate(forecastData.date),
+            };
+          }
+        );
         setCurrentWeather(tempCurrWeather);
         setHourlyWeather(tempHourWeather);
+        setForecastWeather(tempForecastWeather);
       })
       .catch((err) => {
         console.log(err);
@@ -71,131 +105,203 @@ const Weather = () => {
   }, [location]);
   return (
     <>
-      {currentWeather && hourlyWeather && (
-        <div className="weather-container">
-          <div className="left-container">
-            <div className="go-back-home-button" onClick={() => navigate("/")}>
-              <i
-                className="bi bi-arrow-left"
-                style={{ color: "white", fontSize: "30px" }}
-              ></i>
-              <p className="go-back-text"> {"    "}Search for Another City</p>
-            </div>
-            {currentWeather && (
-              <div className="current-temp-container">
-                <span className="current-temp">
-                  {`${
-                    unitToggle == "F"
-                      ? currentWeather.temp_f
-                      : currentWeather.temp_c
-                  }°`}
-                </span>
-                <div className="loc-date-container">
-                  <p className="location">{currentWeather.location}</p>
-                  <div className="current-time">{currentWeather.time}</div>
+      <>
+        {currentWeather && hourlyWeather && forecastWeather ? (
+          <div className="weather-container">
+            <div className="left-container">
+              <div
+                className="go-back-home-button"
+                onClick={() => navigate("/")}
+              >
+                <i
+                  className="bi bi-arrow-left"
+                  style={{ color: "white", fontSize: "30px" }}
+                ></i>
+                <p className="go-back-text"> {"    "}Search for Another City</p>
+              </div>
+              {currentWeather && (
+                <div className="current-temp-container">
+                  <span className="current-temp">
+                    {`${
+                      unitToggle == "F"
+                        ? currentWeather.temp_f
+                        : currentWeather.temp_c
+                    }°`}
+                  </span>
+                  <div className="loc-date-container">
+                    <p className="location">{currentWeather.location}</p>
+                    <div className="current-time">{currentWeather.time}</div>
+                  </div>
+                  <div className="current-weather-icon">
+                    <img
+                      src={currentWeather.icon}
+                      style={{ backgroundColor: "none" }}
+                    />
+                  </div>
                 </div>
-                <div className="current-weather-icon">
-                  <img
-                    src={currentWeather.icon}
-                    style={{ backgroundColor: "none" }}
-                  />
+              )}
+            </div>
+            <div className="right-container">
+              <div className="current-temp-readings">
+                <div className="right-heading-container">
+                  <p className="right-heading">Current Weather Details</p>
+                  <p className="unit">Unit: </p>
+                  <div
+                    className="form-check form-check-inline"
+                    onClick={() => setUnitToggle("C")}
+                  >
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="unitOptions1"
+                      id="celsius"
+                      checked={unitToggle === "C"}
+                      value="C"
+                    />
+                    <label class="form-check-label" for="unitOptions1">
+                      Celsius
+                    </label>
+                  </div>
+                  <div
+                    class="form-check form-check-inline"
+                    onClick={() => setUnitToggle("F")}
+                  >
+                    <input
+                      class="form-check-input"
+                      type="radio"
+                      name="unitOptions2"
+                      id="farenheit"
+                      checked={unitToggle === "F"}
+                      value="F"
+                    />
+                    <label class="form-check-label" for="unitOptions2">
+                      Farenheit
+                    </label>
+                  </div>
+                </div>
+                <p className="weather-description">
+                  {currentWeather.description}
+                </p>
+                <div className="current-conditions">
+                  <p className="description">Temp max</p>
+                  <p className="unit">{`${
+                    unitToggle === "C"
+                      ? currentWeather.temp_max_c
+                      : currentWeather.temp_max_f
+                  }°`}</p>
+                  <span className="icon">
+                    <i
+                      className="bi bi-thermometer-half"
+                      style={{ fontSize: "24px", color: "red" }}
+                    ></i>
+                  </span>
+                </div>
+                <div className="current-conditions">
+                  <p className="description">Temp min</p>
+                  <p className="unit">{`${
+                    unitToggle === "C"
+                      ? currentWeather.temp_min_c
+                      : currentWeather.temp_min_f
+                  }°`}</p>
+                  <span className="icon">
+                    <i
+                      className="bi bi-thermometer-half"
+                      style={{ fontSize: "24px", color: "#0066cc" }}
+                    ></i>
+                  </span>
+                </div>
+                <div className="current-conditions">
+                  <p className="description">Humidity</p>
+                  <p className="unit">{`${currentWeather.humidity}%`}</p>
+                  <span className="icon">
+                    <i
+                      className="bi bi-droplet-half"
+                      style={{ fontSize: "24px", color: "white" }}
+                    ></i>
+                  </span>
+                </div>
+                <div className="current-conditions">
+                  <p className="description">Cloud</p>
+                  <p className="unit">{`${currentWeather.cloud}%`}</p>
+                  <span className="icon">
+                    <i
+                      className="bi bi-clouds"
+                      style={{ fontSize: "24px", color: "white" }}
+                    ></i>
+                  </span>
+                </div>
+                <div className="current-conditions">
+                  <p className="description">Wind</p>
+                  <p className="unit">{`${currentWeather.wind_kph}Km/h`}</p>
+                  <span className="icon">
+                    <i
+                      className="bi bi-wind"
+                      style={{ fontSize: "24px", color: "white" }}
+                    ></i>
+                  </span>
                 </div>
               </div>
-            )}
-          </div>
-          <div className="right-container">
-            <div className="current-temp-readings">
-              <p className="right-heading">Current Weather Details</p>
-              <p className="weather-description">
-                {currentWeather.description}
-              </p>
-              <div className="current-conditions">
-                <p className="description">Temp max</p>
-                <p className="unit">{`${
-                  unitToggle === "C"
-                    ? currentWeather.temp_max_c
-                    : currentWeather.temp_max_f
-                }°`}</p>
-                <span className="icon">
-                  <i
-                    className="bi bi-thermometer-half"
-                    style={{ fontSize: "24px", color: "red" }}
-                  ></i>
-                </span>
-              </div>
-              <div className="current-conditions">
-                <p className="description">Temp min</p>
-                <p className="unit">{`${
-                  unitToggle === "C"
-                    ? currentWeather.temp_min_c
-                    : currentWeather.temp_min_f
-                }°`}</p>
-                <span className="icon">
-                  <i
-                    className="bi bi-thermometer-half"
-                    style={{ fontSize: "24px", color: "#0066cc" }}
-                  ></i>
-                </span>
-              </div>
-              <div className="current-conditions">
-                <p className="description">Humidity</p>
-                <p className="unit">{`${currentWeather.humidity}%`}</p>
-                <span className="icon">
-                  <i
-                    className="bi bi-droplet-half"
-                    style={{ fontSize: "24px", color: "white" }}
-                  ></i>
-                </span>
-              </div>
-              <div className="current-conditions">
-                <p className="description">Cloud</p>
-                <p className="unit">{`${currentWeather.cloud}%`}</p>
-                <span className="icon">
-                  <i
-                    className="bi bi-clouds"
-                    style={{ fontSize: "24px", color: "white" }}
-                  ></i>
-                </span>
-              </div>
-              <div className="current-conditions">
-                <p className="description">Wind</p>
-                <p className="unit">{`${currentWeather.wind_kph}Km/h`}</p>
-                <span className="icon">
-                  <i
-                    className="bi bi-wind"
-                    style={{ fontSize: "24px", color: "white" }}
-                  ></i>
-                </span>
-              </div>
-            </div>
-            <div className="vertical-divider"></div>
-            <div className="hourly-weather">
-              {hourlyWeather.map((hw) => {
-                return (
-                  <>
-                    <div className="hourly-reading">
-                      <span className="icon">
-                        <img
-                          src={hw.icon}
-                          style={{ backgroundColor: "none" }}
-                        />
-                      </span>
-                      <div className="time-desc-container">
-                        <p className="time">{hw.time}</p>
-                        <p className="description">{hw.description}</p>
+              <div className="vertical-divider"></div>
+              <p className="hourly-forecast-heading">Hourly Forecast</p>
+              <div className="hourly-weather">
+                {hourlyWeather.map((hw) => {
+                  return (
+                    <>
+                      <div className="hourly-reading">
+                        <span className="icon">
+                          <img
+                            src={hw.icon}
+                            style={{ backgroundColor: "none" }}
+                          />
+                        </span>
+                        <div className="time-desc-container">
+                          <p className="time">{hw.time}</p>
+                          <p className="description">{hw.description}</p>
+                        </div>
+                        <p className="temp">{`${
+                          unitToggle === "C" ? hw.temp_c : hw.temp_f
+                        }°`}</p>
                       </div>
-                      <p className="temp">{`${
-                        unitToggle === "C" ? hw.temp_c : hw.temp_f
-                      }°`}</p>
-                    </div>
-                  </>
-                );
-              })}
+                    </>
+                  );
+                })}
+              </div>
+              <div className="vertical-divider"></div>
+              <p className="day-forecast-heading">5 Day Forecast</p>
+              <div className="forecast-weather">
+                {forecastWeather.map((fw) => {
+                  return (
+                    <>
+                      <div className="day-reading">
+                        <span className="icon">
+                          <img
+                            src={fw.icon}
+                            alt=""
+                            style={{ backgroundColor: "none" }}
+                          />
+                        </span>
+                        <p className="time">{fw.time}</p>
+                        <div className="max-min-temp-container">
+                          <p className="min-temp">{`Min- ${
+                            unitToggle === "C" ? fw.mintemp_c : fw.mintemp_f
+                          }°`}</p>
+                          <p className="max-temp">{`Max- ${
+                            unitToggle === "C" ? fw.maxtemp_c : fw.maxtemp_f
+                          }°`}</p>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
             </div>
-            <div className="vertical-divider"></div>
           </div>
-        </div>
-      )}
+        ) : (
+          <>
+            <div class="spinner-border text-primary" role="status"></div>
+          </>
+        )}
+      </>
     </>
   );
 };
